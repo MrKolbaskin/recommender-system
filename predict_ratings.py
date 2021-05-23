743c426faa8200481a4a10de6da4756c52a67d86
Vladimir Dubinin <vladimir.dubinin@bostongene.com>
2021-05-24 00:52:12 +0300

    Updated predict ratings

--- predict_ratings.py
+++ predict_ratings.py
@@ -1,15 +1,34 @@
-from surprise import dump
 import pandas as pd
 from termcolor import colored
 from tqdm import tqdm
+import numpy as np
+from collections import namedtuple
+
+
+USERS_FILE = "static/svd_embedings/users_svd.pkl"
+MOVIES_FILE = "static/svd_embedings/movies_svd.pkl"
+df_ratings = pd.read_csv("static/data/ratings_predict.csv")
+
+
+Prediction = namedtuple("Prediction", ["uid", "iid", "est"])
+
+
+class SVDModel:
+    def __init__(self, users, movies):
+        self.users = users
+        self.movies = movies
+
+    def predict(self, user_id: int, movie_id: int):
+        return Prediction(
+            user_id, movie_id, self.users[user_id] @ self.movies[movie_id]
+        )
 
 
 def prediction_to_dict(prediction):
     return {
-        "UserId": prediction.uid,
-        "MovieId": prediction.iid,
-        "True rating": prediction.r_ui,
-        "Prediction rating": prediction.est,
+        "Номер пользователя": prediction.uid,
+        "Номер фильма": prediction.iid,
+        "Предсказанный рейтинг": prediction.est,
     }
 
 
@@ -22,19 +41,15 @@ def predictions_to_df(predictions):
 if __name__ == "__main__":
     try:
         print("LOAD SVD MODEL")
-        _, svd_model = dump.load("static/models/svd.model")
-        df_ratings = pd.read_csv("static/data/ratings.csv")
-
-        test_ratings = list(
-            zip(
-                df_ratings["userId"].to_list(),
-                df_ratings["movieId"].to_list(),
-                df_ratings["rating"].to_list(),
-            )
-        )
+        users = np.load(USERS_FILE, allow_pickle=True)
+        movies = np.load(MOVIES_FILE, allow_pickle=True)
+        svd_model = SVDModel(users, movies)
 
         print("PREDICT")
-        predictions = svd_model.test(test_ratings)
+        predictions = []
+        for user_id in tqdm(range(len(users))):
+            for movie_id in range(len(movies)):
+                predictions.append(svd_model.predict(user_id, movie_id))
 
         print("TRANSLATE")
         df_predictions = predictions_to_df(predictions)
@@ -44,4 +59,4 @@ if __name__ == "__main__":
         print(colored("Что-то пошло не так!", "red"))
         exit()
 
-    print(colored("Предсказания находятся в файле predictions.csv", "green"))
+    print(colored("Предсказания находятся в файле ratings_predict.csv", "green"))
